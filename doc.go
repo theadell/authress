d5290b8b25cbc70274e9e7c024c6a89941efb738
adel <65723807+theadell@users.noreply.github.com>
2024-09-12 11:00:47 +0200

    add package doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Authress is a lightweight package for adding token validation and introspection to resource servers.
+It supports OAuth2/OIDC discovery and custom keys & JWT parsing and validation
+the package also provides easily customizable and pluggable middleware for integrating authentication into net/http handlers.
+
+See the README for usage and examples.
+*/
+package authress
